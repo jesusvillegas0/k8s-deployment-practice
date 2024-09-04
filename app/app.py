b75b765d3042498eeb3dbe3aca170ca7241a8759
jesusvillegas0 <jesusvillegas0@gmail.com>
2024-09-04 18:34:59 -0400

    Chancho Feliz

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Kubernetes, nueva version 2.0! Version: ' + os.environ.get('APP_VERSION', '2.0.1')
+    return 'Hello, Kubernetes, Feliz Chanchito Version: ' + os.environ.get('APP_VERSION', '2.0.1')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
